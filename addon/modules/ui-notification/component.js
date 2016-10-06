a62a882bc335cd0beaacbd3562bef484aa0084dd
余凡 <albert@cform.io>
2016-10-06 16:21:05 +0800

    Remove example code out of ui-notification

--- addon/modules/ui-notification/component.js
+++ addon/modules/ui-notification/component.js
@@ -25,22 +25,6 @@ export default Component.extend({
     return get(this, `styles.${get(this, 'position')}`);
   }),
 
-  init() {
-    this._super(...arguments);
-    this.notification.info('<strong>This is an example message!!!</strong>', {
-      autoClear: 0
-    });
-
-    window.setTimeout(() => {
-      this.notification.success('<strong>这是一条提醒消息的范例！！！</strong>');
-    }, 5000)
-
-    this.notification.warning('<strong>This is an example message!!!</strong>');
-    this.notification.error('<strong>这是一条提醒消息的范例！！！</strong>', {
-      autoClear: 10000
-    });
-  },
-
   actions: {
     close(instance) {
       this.notification.remove(instance);
---
+++ tests/dummy/app/modules/notification/controller.js
@@ -0,0 +1,21 @@
+import Controller from 'ember-controller';
+import { later } from 'ember-runloop';
+
+export default Controller.extend({
+  init() {
+    this.notification.info('<strong>This is an example message!!!</strong>', {
+      autoClear: 0
+    });
+
+    later(this, function() {
+      this.notification.success('<strong>这是一条提醒消息的范例！！！</strong>');
+    }, 5000);
+
+    this.notification.warning('<strong>This is an example message!!!</strong>');
+    this.notification.error('<strong>这是一条提醒消息的范例！！！</strong>', {
+      autoClear: 10000
+    });
+
+    this._super(...arguments);
+  },
+});
