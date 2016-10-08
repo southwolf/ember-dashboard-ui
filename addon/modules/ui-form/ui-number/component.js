b185d539c4652571bdc782c4df16537996f920f2
余凡 <albert@cform.io>
2016-10-08 20:08:34 +0800

    Refactor ui-number component

--- addon/modules/ui-form/ui-number/component.js
+++ addon/modules/ui-form/ui-number/component.js
@@ -1,16 +1,44 @@
 import Component from 'ember-component';
-import UIFormWrapperMixin from '../../../mixins/ui-form-wrapper';
 import layout from './template';
-import styles from '../styles';
 import computed, { reads } from 'ember-computed';
+import { isPresent } from 'ember-utils';
+import { htmlSafe } from 'ember-string';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 
-export default Component.extend(UIFormWrapperMixin, {
-  layout, styles,
+export default Component.extend({
+  layout,
+  classNames: ['ui-number'],
   classNameBindings: ['disabled'],
 
   hasDisabled: reads('disabled'),
+
+  hasLabel: computed('label', function() {
+    return isPresent(get(this, 'label'));
+  }),
+
+  labelContent: computed('hasLabel', function() {
+    const label = get(this, 'label');
+    if (get(this, 'hasLabel')) {
+      return htmlSafe(`<label>${label}</label>`);
+    } else return '';
+  }),
+
+  errorContent: computed('subject.error', function() {
+    const error = get(this, 'subject.error');
+    if (isPresent(error)) {
+      return htmlSafe(`<div class="field-error">${error}</div>`)
+    }
+  }),
+
+  didInsertElement() {
+    if (get(this, 'hasLabel')) {
+      const input = this.element.querySelector('input');
+      const label = this.element.querySelector('label');
+      label.setAttribute('for', input.id);
+      this.element.parentElement.classList.add('has-label');
+    }
+  },
 
   // check if value is valid against max
   isValidToMax: computed('max', 'value', function() {
@@ -26,15 +54,6 @@ export default Component.extend(UIFormWrapperMixin, {
   normalized: computed('value', 'step', function() {
     return get(this, 'value') % get(this, 'step') === 0;
   }),
-
-  didInsertElement() {
-    if (!get(this, 'hasSplit')) {
-      if (get(this, 'label')) {
-        const input = document.getElementById(this.fieldId);
-        input.parentNode.classList.add('has-label');
-      }
-    }
-  },
 
   actions: {
     increase() {
