bd70066953a0725f262bc1cb2742d4b6a9e91896
余凡 <albert@cform.io>
2016-10-09 15:13:38 +0800

    ui-checkbox and ui-raido don't need has-label class name

--- addon/modules/ui-form/ui-radio/component.js
+++ addon/modules/ui-form/ui-radio/component.js
@@ -28,6 +28,5 @@ export default Component.extend({
     const input = this.element.querySelector('input');
     const label = this.element.querySelector('label');
     label.setAttribute('for', input.id);
-    this.element.parentElement.classList.add('has-label');
   }
 });
