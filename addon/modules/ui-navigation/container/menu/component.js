424c1d333119987c2d584944bb9f4b4bb77b5fc2
余凡 <albert@cform.io>
2016-10-12 23:42:49 +0800

    Add menu support for ui-navigation

---
+++ addon/modules/ui-navigation/container/menu/component.js
@@ -0,0 +1,13 @@
+import Component from 'ember-component';
+import layout from './template';
+import styles from '../../styles';
+import { equal } from 'ember-computed';
+
+export default Component.extend({
+  layout, styles,
+  tagName: 'ul',
+  localClassNames: ['menu'],
+  localClassNameBindings: ['pullRight:r'],
+
+  pullRight: equal('position', 'r')
+}).reopenClass({ positionalParams: ['items'] });
---
+++ tests/dummy/app/modules/page/route.js
@@ -0,0 +1,17 @@
+import Route from 'ember-route';
+import { setProperties } from 'ember-metal/set';
+
+export default Route.extend({
+  setupController(controller) {
+    this._super(...arguments);
+    setProperties(controller, {
+      menuOne: [
+        { name: '首页', route: 'index' },
+        { name: '导航', route: 'page' },
+        { name: '拖拽', route: 'drag-and-drop' },
+        { name: '表单', route: 'form' },
+        { name: '通知', route: 'notification' },
+      ]
+    })
+  }
+});
