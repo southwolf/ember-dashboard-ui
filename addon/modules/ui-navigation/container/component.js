e3f4c900cb39500f8c4d2430a7f221c9438e3dba
余凡 <albert@cform.io>
2016-10-12 21:55:53 +0800

    Add logo to ui-navigation

--- addon/modules/ui-navigation/container/component.js
+++ addon/modules/ui-navigation/container/component.js
@@ -1,7 +1,13 @@
 import Component from 'ember-component';
+import layout from './template';
 import styles from '../styles';
+import { equal } from 'ember-computed';
 
 export default Component.extend({
-  styles, tagName: 'nav',
-  attributeBindings: ['style']
-});
+  layout, styles,
+  tagName: 'nav',
+  attributeBindings: ['style'],
+
+  isPrimary: equal('level', 'primary'),
+  isSecondary: equal('level', 'secondary')
+}).reopenClass({ positionalParams: ['level'] });
