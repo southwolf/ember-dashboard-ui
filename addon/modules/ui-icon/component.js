7d5270adbb734282daaa35e4e308f44c7c775629
Wester <wester@cform.io>
2016-10-21 04:08:11 +0800

    svg icon styles

--- addon/modules/ui-icon/component.js
+++ addon/modules/ui-icon/component.js
@@ -1,12 +1,13 @@
 import Component from 'ember-component';
 import layout from './template';
+import styles from './styles';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
 import { htmlSafe } from 'ember-string';
 import { tryInvoke } from 'ember-utils';
 
 export default Component.extend({
-  layout,
+  layout, styles,
   tagName: '',
   size: 16,
 
