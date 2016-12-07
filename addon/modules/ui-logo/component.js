651142b80fd1e66acac305fe4f2d2a85e4871e87
余凡 <albert@cform.io>
2016-12-07 18:53:40 +0800

    Enable to replace svg in ui-logo

--- addon/modules/ui-logo/component.js
+++ addon/modules/ui-logo/component.js
@@ -2,9 +2,17 @@ import Ember from 'ember';
 import layout from './template';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import computed from 'ember-computed';
+import { isNone } from 'ember-utils';
+import { htmlSafe } from 'ember-string';
 
 export default Ember.LinkComponent.extend({
   layout,
+
+  linkLabel: computed('linkTitle', function() {
+    return isNone(get(this, 'linkTitle'))
+      ? null : htmlSafe(`<span>${get(this, 'linkTitle')}`)
+  }).readOnly(),
 
   willRender() {
     let params = get(this, 'params');
