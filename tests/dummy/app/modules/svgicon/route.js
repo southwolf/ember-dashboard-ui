ba89061439f7886a61140681912c5af7bfdfb896
Wester <wester@cform.io>
2016-12-10 18:33:05 +0800

    2.28

--- tests/dummy/app/modules/svgicon/route.js
+++ tests/dummy/app/modules/svgicon/route.js
@@ -89,6 +89,7 @@ export default Route.extend({
         {icon: "tips"},
         {icon: "print"},
         {icon: "chat"},
+        {icon: "log-out"},
       ],
       reports: [
         {icon: "ui-filter"},
